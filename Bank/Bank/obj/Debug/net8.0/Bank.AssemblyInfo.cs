1104242d9ac2c91ade70771b36a1d38af88586a4
Orangelos <Orangelos@bk.ru>
2024-06-03 20:38:17 +0300

    fff

--- Bank/Bank/obj/Debug/net8.0/Bank.AssemblyInfo.cs
+++ Bank/Bank/obj/Debug/net8.0/Bank.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c8d1c24d3e02297429288f9c1b4bfbff15a68f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63d28fe9bac0342b0e14dada786efe6f1c024e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
